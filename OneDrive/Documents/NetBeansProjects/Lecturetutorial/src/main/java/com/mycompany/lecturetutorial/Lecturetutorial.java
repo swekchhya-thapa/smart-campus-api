96a8178d9065dafa8a4ef923ef43aae620d9068b
Sweknene <Swekchhya123@gmail.com>
2025-01-31 18:18:01 +0545

    yess yess

---
+++ OneDrive/Documents/NetBeansProjects/Lecturetutorial/src/main/java/com/mycompany/lecturetutorial/Lecturetutorial.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lecturetutorial;
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class Lecturetutorial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Scanner s = new Scanner(System.in);
+        System.out.print("Insert your name:");
+        String s1 = s.next();
+        String s2 = s.next();
+        System.out.println("Hello"+s1+" "+s2);
+        
+    }
+}
